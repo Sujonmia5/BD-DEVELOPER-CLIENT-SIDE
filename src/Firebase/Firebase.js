fd0a4730282d6c6e7bf4c531b5d4feeafc457911
Sujonmia5 <mdsujon673738@gmail.com>
2022-10-26 00:20:17 +0600

    firebase key  hide

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBMmPKrQq1eI95f2cIh7vz8DZFNsp0co2o",
-    authDomain: "bd-developers-7438e.firebaseapp.com",
-    projectId: "bd-developers-7438e",
-    storageBucket: "bd-developers-7438e.appspot.com",
-    messagingSenderId: "925554352699",
-    appId: "1:925554352699:web:0a804233ce9aab82cef21e"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
